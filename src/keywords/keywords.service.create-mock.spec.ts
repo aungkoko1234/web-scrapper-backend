7f11174f56b7547a3c2311364ce387c4ccf87452
aungkoko1234 <aungkokouit2096@gmail.com>
2023-01-02 15:21:16 +0630

    chore - fixed test suites for keyword

---
+++ src/keywords/keywords.service.create-mock.spec.ts
@@ -0,0 +1,78 @@
+import { createMock } from '@golevelup/ts-jest';
+import { Test } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { KeyWord } from './entity/keyword.entity';
+import { KeywordsService } from './keywords.service';
+
+describe('KeyWords using createMock with DI', () => {
+  let repo: Repository<KeyWord>;
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        KeywordsService,
+        {
+          provide: getRepositoryToken(KeyWord),
+          useValue: createMock<Repository<KeyWord>>(),
+        },
+        {
+          provide: 'db_read_KeyWordRepository',
+          useValue: createMock<Repository<KeyWord>>(),
+        },
+        {
+          provide: 'db_write_KeyWordRepository',
+          useValue: createMock<Repository<KeyWord>>(),
+        },
+      ],
+    }).compile();
+
+    // service = module.get<KeywordsService>(KeywordsService);
+    repo = module.get<Repository<KeyWord>>(getRepositoryToken(KeyWord));
+  });
+
+  it('should have the repo mocked', () => {
+    expect(typeof repo.find).toBe('function');
+  });
+});
+
+describe('KeywordsService using createMock without DI', () => {
+  const repo = createMock<Repository<KeyWord>>();
+
+  beforeEach(async () => {
+    await Test.createTestingModule({
+      providers: [
+        KeywordsService,
+        {
+          provide: getRepositoryToken(KeyWord),
+          useValue: repo,
+        },
+        {
+          provide: 'db_read_KeyWordRepository',
+          useValue: createMock<Repository<KeyWord>>(),
+        },
+        {
+          provide: 'db_write_KeyWordRepository',
+          useValue: createMock<Repository<KeyWord>>(),
+        },
+      ],
+    }).compile();
+  });
+
+  it('should have the repo mocked', async () => {
+    const keyword = {
+      id: 'bh781z',
+      adsWordCount: 20,
+      createdBy: '1abcdef',
+      htmlSource: '',
+      linkCount: 70,
+      name: 'Job',
+      searchResultCount: '2300000',
+      created: new Date(),
+      updated: new Date(),
+    };
+    repo.find.mockResolvedValue([keyword]);
+    // tslint:disable-next-line: no-invalid-await
+    expect(await repo.find()).toEqual([keyword]);
+  });
+});
--- src/keywords/keywords.service.spec.ts
+++ src/keywords/keywords.service.spec.ts
@@ -1,15 +1,77 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { KeywordsService } from './keywords.service';
+import { KeyWord } from './entity/keyword.entity';
+import { createMock } from '@golevelup/ts-jest';
+
+const keywordArray = [
+  {
+    adsWordCount: 20,
+    createdBy: '1abcdef',
+    htmlSource: '',
+    linkCount: 70,
+    name: 'Job',
+    searchResultCount: '2300000',
+  },
+  {
+    adsWordCount: 21,
+    createdBy: '1abcdeg',
+    htmlSource: '',
+    linkCount: 70,
+    name: 'Flight Ticket',
+    searchResultCount: '2300000',
+  },
+];
+
+const oneKeyword = {
+  id: '1fghij',
+  adsWordCount: 22,
+  createdBy: '1abcdeh',
+  htmlSource: '',
+  linkCount: 70,
+  name: 'Flight Ticket',
+  searchResultCount: '2300000',
+};
 
 describe('KeywordsService', () => {
   let service: KeywordsService;
+  let repo: Repository<KeyWord>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [KeywordsService],
+      providers: [
+        KeywordsService,
+        {
+          provide: 'db_read_KeyWordRepository',
+          useValue: createMock<Repository<KeyWord>>(),
+        },
+        {
+          provide: 'db_write_KeyWordRepository',
+          useValue: createMock<Repository<KeyWord>>(),
+        },
+        {
+          provide: getRepositoryToken(KeyWord),
+          // define all the methods that you use from the catRepo
+          // give proper return values as expected or mock implementations, your choice
+          useValue: {
+            find: jest.fn().mockResolvedValue(keywordArray),
+            findOne: jest.fn().mockResolvedValue(oneKeyword),
+            create: jest.fn().mockReturnValue(oneKeyword),
+            // save: jest.fn(),
+            // // as these do not actually use their return values in our sample
+            // // we just make sure that their resolve is true to not crash
+            // update: jest.fn().mockResolvedValue(true),
+            // // as these do not actually use their return values in our sample
+            // // we just make sure that their resolve is true to not crash
+            // delete: jest.fn().mockResolvedValue(true),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<KeywordsService>(KeywordsService);
+    repo = module.get<Repository<KeyWord>>(getRepositoryToken(KeyWord));
   });
 
   it('should be defined', () => {
